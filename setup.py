bbf297870f53451962efc7deb66a855892583876
Pawel Chomicki <pawel.chomicki@gmail.com>
2014-03-12 21:31:43 +0100

    Update package description

--- setup.py
+++ setup.py
@@ -13,9 +13,9 @@ requires = [
 setup(
     name="pytest_spec",
     packages=find_packages(),
-    version="0.2.6",
+    version="0.2.7",
     entry_points={'pytest11': ['pytest_spec = pytest_spec.plugin']},
-    description="Pytest plugin to print more spec report than only execution result",
+    description="pytest plugin to display test execution output like a SPECIFICATION",
     author="Pawel Chomicki",
     author_email="pawel.chomicki@gmail.com",
     install_requires=requires,
