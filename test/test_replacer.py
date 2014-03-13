02378adca12511b355abdd600c7173801e268cf1
Pawel Chomicki <pawel.chomicki@gmail.com>
2014-03-13 23:18:49 +0100

    Add test files for patch and replacer modules

---
+++ test/test_replacer.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+:author: Pawel Chomicki
+"""
+import unittest
+
+
+class TestPatcher(unittest.TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
