d13c588c41cb0b7274dfd60fd9a3a6a738f3ceeb
Pawel Chomicki <pawel.chomicki@gmail.com>
2014-03-15 21:06:35 +0100

    close #3: Bump version to 0.2.14

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requires = [
 setup(
     name="pytest_spec",
     packages=find_packages(),
-    version="0.2.13",
+    version="0.2.14",
     entry_points={'pytest11': ['pytest_spec = pytest_spec.plugin']},
     description="pytest plugin to display test execution output like a SPECIFICATION",
     author="Pawel Chomicki",
