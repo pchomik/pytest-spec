face54637c6ede6d971757d02142b614658c8a2b
Vasili Syrakis <vsyrakis@atlassian.com>
2019-12-09 10:47:57 +1100

    Fix pytest UserWarning
    pytest_spec/patch.py:23: UserWarning: Argument(s) ('config',) which are declared in the hookspec can not be found in this hook call
    res = self.config.hook.pytest_report_teststatus(report=report)
    
    -- Docs: https://docs.pytest.org/en/latest/warnings.html

--- pytest_spec/patch.py
+++ pytest_spec/patch.py
@@ -20,7 +20,7 @@ def pytest_runtest_logreport(self, report):
 
     Hook changed to define SPECIFICATION like output format. This hook will overwrite also VERBOSE option.
     """
-    res = self.config.hook.pytest_report_teststatus(report=report)
+    res = self.config.hook.pytest_report_teststatus(report=report, config=self.config)
     cat, letter, word = res
     self.stats.setdefault(cat, []).append(report)
     if not letter and not word:
