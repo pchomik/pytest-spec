39a18d8bc762d3c1513b4204b59bfef03401aa66
Pawel Chomicki <pawel.chomicki@gmail.com>
2019-12-17 21:48:25 +0100

    Add new test to check describe like tests
    Closes #22

---
+++ test/test_formats/test_describe_format.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+:author: Pawel Chomicki
+:e-mail: pawel.chomicki@gmail.com
+"""
+import unittest
+
+
+def describe_first_level():
+
+    def it_passed_on_first_level():
+        assert True is True
+
+    @unittest.skip('To implement')
+    def it_skipped_on_first_level():
+        assert True is True
+
+    @unittest.skip('Remove docorator to see fail result')
+    def it_failed_on_first_level():
+        assert True is False
+
+    def describe_second_level():
+
+        def it_passed_on_second_level():
+            assert True is True
+
+        @unittest.skip('To implement')
+        def it_skipped_on_second_level():
+            assert True is True
+
+        @unittest.skip('Remove docorator to see fail result')
+        def it_failed_on_second_level():
+            assert True is False
+
+        def describe_third_level():
+
+            def it_passed_on_third_level():
+                assert True is True
+
+            @unittest.skip('To implement')
+            def it_skipped_on_third_level():
+                assert True is True
+
+            @unittest.skip('Remove docorator to see fail result')
+            def it_failed_on_third_level():
+                assert True is False
+
+
+
+if __name__ == '__main__':
+    unittest.main()
