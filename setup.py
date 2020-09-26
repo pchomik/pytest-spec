6500312f195ac19859018d885534e1cf574891ff
Pawel Chomicki <pawel.chomicki@gmail.com>
2020-09-26 22:13:26 +0200

    Remove encoding not valid for python 2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+with open(path.join(this_directory, 'README.rst')) as f:
     long_description = f.read()
 
 
