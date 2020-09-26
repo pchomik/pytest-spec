8672501833d18283960c4c223504b998c8c79a59
Pawel Chomicki <pawel.chomicki@gmail.com>
2020-09-26 21:57:00 +0200

    Add licence and long_description package setup

--- setup.py
+++ setup.py
@@ -2,7 +2,14 @@
 """
 :author: Pawel Chomicki
 """
+from os import path
+
 from setuptools import setup, find_packages
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 
 requires = [
@@ -16,8 +23,12 @@ setup(
     version="3.0.1",
     entry_points={'pytest11': ['pytest_spec = pytest_spec.plugin']},
     description="pytest plugin to display test execution output like a SPECIFICATION",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
     author="Pawel Chomicki",
     author_email="pawel.chomicki@gmail.com",
     install_requires=requires,
     url="https://github.com/pchomik/pytest-spec",
+    license='GPLv2',
+    license_file='LICENSE.txt'
 )
